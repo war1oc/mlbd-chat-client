aa0d321dbe03dd882b2b0ca30363cd9fe736b57e
Tanveer Hassan <tanveer_hassan@outlook.com>
2019-09-26 23:22:03 +0600

    fix: fix test during push

---
+++ test/chat-client.test.ts
@@ -0,0 +1,8 @@
+/**
+ * ChatClient test
+ */
+describe('ChatClient test', () => {
+  it('passes this test', () => {
+    expect(true).toBeTruthy()
+  })
+})
--- test/chat-js-client.test.ts
+++
@@ -1,8 +0,0 @@
-/**
- * ChatClient test
- */
-describe("ChatClient test", () => {
-  it("passes this test", () => {
-    expect(true).toBeTruthy();
-  });
-});
