37bafe011640a7a8acf3ccb7cb2ed26681dc8782
Tanveer Hassan <tanveer.hassan@sekai-lab-bd.com>
2020-08-07 17:45:12 +0600

    chore: update npm dependencies

--- src/pusher.ts
+++ src/pusher.ts
@@ -1,10 +1,10 @@
-import * as Pusher from 'pusher-js'
+import Pusher from 'pusher-js'
 import { TokenProvider } from './token-provider'
 
 export class PusherProvider {
   public options: IPusherProviderOptions
   private userId: string
-  private pusher: Pusher.Pusher
+  private pusher: Pusher
 
   constructor(options: IPusherProviderOptions) {
     this.options = options
@@ -13,15 +13,15 @@ export class PusherProvider {
     this.pusher = new Pusher(this.options.appKey, {
       cluster: this.options.cluster,
       authEndpoint: this.options.authEndpoint,
-      forceTLS: this.options.forceTLS
+      forceTLS: this.options.forceTLS,
     })
   }
 
   public async connect(userId: string) {
     this.pusher.config.auth = {
       headers: {
-        Authorization: `Bearer ${await this.options.tokenProvider.getAuthToken()}`
-      }
+        Authorization: `Bearer ${await this.options.tokenProvider.getAuthToken()}`,
+      },
     }
 
     this.pusher.subscribe(`private-${userId}`)
