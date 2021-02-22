15e596ce2d554f8b3f344aed02ba2dc0c413a5bd
tanvir raj <tanvir.raj@monstar-lab.com>
2021-02-22 21:29:18 +0600

    feat: test initialize and test my group messages

--- test/chat-client.test.ts
+++ test/chat-client.test.ts
@@ -2,18 +2,46 @@
  * ChatClient test
  */
 import { ChatClient } from '../src/chat-client'
+import { post, put } from '../src/request'
+
+jest.mock('../src/request')
+const mockPost = post as jest.Mock
 
 describe('Chat Client Class', function () {
-  it('should create a new Chat client', function () {
-    const tokenProvider: any = {}
+  let chatClient: ChatClient
+  beforeEach(async () => {
+    const tokenProvider: any = { getAuthToken: jest.fn().mockReturnValue('tokenProvider') }
     const pusherProvider: any = {}
 
-    const chatClient = new ChatClient({
+    chatClient = new ChatClient({
       chatApiEndpoint: 'https://my-chat-api',
       tokenProvider: tokenProvider,
       pusherProvider: pusherProvider,
     })
+  })
 
+  it('should be defined', () => {
     expect(chatClient).toBeDefined()
+  })
+
+  describe('get My groups', () => {
+    it('should return correct groups', async () => {
+      const users = [{ name: 'Bob' }]
+      const resp = users
+      mockPost.mockImplementation(() => Promise.resolve(resp))
+      const myGroups = await chatClient.getMyGroups()
+      expect(myGroups).toBe(users)
+    })
+
+    it('should call with token which is provided', async () => {
+      const users = [{ name: 'Bob' }]
+      const resp = users
+      mockPost.mockImplementation(() => Promise.resolve(resp))
+      const myGroups = await chatClient.getMyGroups()
+      expect(mockPost).toHaveBeenCalledTimes(2)
+      expect(mockPost).toHaveBeenLastCalledWith('https://my-chat-api/groups.list', {
+        token: 'tokenProvider',
+      })
+    })
   })
 })
