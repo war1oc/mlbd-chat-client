5bfd7e678a91bdeaf20658457a4fa84b922ac5da
tanvir raj <tanvir.raj@monstar-lab.com>
2021-02-24 15:47:13 +0600

    feat: add test for promise reject state

--- test/chat-client.test.ts
+++ test/chat-client.test.ts
@@ -7,10 +7,15 @@ import { post, put } from '../src/request'
 jest.mock('../src/request')
 const mockPost = post as jest.Mock
 
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
 describe('Chat Client Class', function () {
   let chatClient: ChatClient
+
   beforeEach(async () => {
-    const tokenProvider: any = { getAuthToken: jest.fn().mockReturnValue('tokenProvider') }
+    const tokenProvider: any = { getAuthToken: jest.fn().mockReturnValue('<CHAT_TOKEN>') }
     const pusherProvider: any = {}
 
     chatClient = new ChatClient({
@@ -24,23 +29,30 @@ describe('Chat Client Class', function () {
     expect(chatClient).toBeDefined()
   })
 
-  describe('get My groups', () => {
+  describe('getMyGroups', () => {
     it('should return correct groups', async () => {
-      const users = [{ name: 'Bob' }]
-      const resp = users
-      mockPost.mockImplementation(() => Promise.resolve(resp))
+      const received = [{ id: '1' }, { id: '2' }]
+      mockPost.mockResolvedValue(received)
       const myGroups = await chatClient.getMyGroups()
-      expect(myGroups).toBe(users)
+      expect(myGroups).toBe(received)
     })
 
-    it('should call post with token ', async () => {
-      const users = [{ name: 'Bob' }]
-      const resp = users
-      mockPost.mockImplementation(() => Promise.resolve(resp))
+    it('should call post with token', async () => {
+      const received = [{ id: '1' }, { id: '2' }]
+      mockPost.mockResolvedValue(received)
       const myGroups = await chatClient.getMyGroups()
       expect(mockPost).toHaveBeenLastCalledWith('https://my-chat-api/groups.list', {
-        token: 'tokenProvider',
+        token: '<CHAT_TOKEN>',
       })
+    })
+
+    it('should return error ', async () => {
+      mockPost.mockImplementation(() => Promise.reject(new Error('error')))
+      try {
+        await chatClient.getMyGroups()
+      } catch (e) {
+        expect(e).toEqual(new Error('error'))
+      }
     })
   })
 })
